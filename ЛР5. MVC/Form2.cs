fc868a359352c8f447b03bf09cf25a75bb5bc105
MOFerik <73346667+MOFerik@users.noreply.github.com>
2020-12-11 03:37:09 +0500

    Комментарии
    Куда же без них :)

--- ЛР5. MVC/Form2.cs
+++ ЛР5. MVC/Form2.cs
@@ -10,19 +10,19 @@ namespace ЛР5.MVC
         {
             InitializeComponent();
 
-            model = new Model();
+            model = new Model();                                                // Создание модели и подключение наблюдателей
             model.observers += new System.EventHandler(this.UpdateFromModel);
         }
 
-        private void TextBox1_TextChanged(object sender, EventArgs e)
+        private void TextBox1_TextChanged(object sender, EventArgs e)           // Обработка изменения значения текстбокса
         {
-            if (textBox1.Text == "" || textBox1.Text == "-")
+            if (textBox1.Text == "" || textBox1.Text == "-")                    // "Не числа"
                 model.setValue1(0);
             else
                 model.setValue1(Convert.ToInt32(textBox1.Text));
         }
 
-        private void TextBox2_TextChanged(object sender, EventArgs e)
+        private void TextBox2_TextChanged(object sender, EventArgs e)           // Другие элементы (принцип тот же)
         {
             if (textBox2.Text == "" || textBox2.Text == "-")
                 model.setValue2(0);
@@ -78,9 +78,9 @@ namespace ЛР5.MVC
             model.setValue2(model.getValue2() - 1);
         }
 
-        private void UpdateFromModel(object sender, EventArgs e)
+        private void UpdateFromModel(object sender, EventArgs e)                        // Обновление отображающих числа элементов
         {
-            if (model.getValue1() >= model.getValue2() && model.getSide() == 1)
+            if (model.getValue1() >= model.getValue2() && model.getSide() == 1)         // Обработка нарушения условия если последние действия производились над числом 1
             {
                 textBox1.Text = Convert.ToString(model.getValue1());
                 numericUpDown1.Value = model.getValue1();
@@ -91,7 +91,7 @@ namespace ЛР5.MVC
                 numericUpDown2.Value = model.getValue2();
                 label2.Text = Convert.ToString(model.getValue2());
             }
-            else if (model.getValue1() >= model.getValue2() && model.getSide() == 2)
+            else if (model.getValue1() >= model.getValue2() && model.getSide() == 2)     // и если над числом 2
             {
                 textBox2.Text = Convert.ToString(model.getValue2());
                 numericUpDown2.Value = model.getValue2();
@@ -116,9 +116,9 @@ namespace ЛР5.MVC
 
     public class Model
     {
-        private int value1;
+        private int value1;                         // Значения чисел 1 и 2 будут храниться здесь
         private int value2;
-        private int lastSide;
+        private int lastSide;                       // Указывает над каким числом выполнили последнюю операцию
         public System.EventHandler observers;
 
         public int getSide()
@@ -126,28 +126,29 @@ namespace ЛР5.MVC
             return lastSide;
         }
 
-        public void setValue1(int value)
+        public void setValue1(int value)            // Изменяет значение числа 1
         {
             this.value1 = value;
             this.lastSide = 1;
             observers.Invoke(this, null);
         }
 
-        public void setValue2(int value)
+        public void setValue2(int value)            // Числа 2
         {
             this.value2 = value;
             this.lastSide = 2;
             observers.Invoke(this, null);
         }
 
-        public int getValue1()
+        public int getValue1()                      // Получение значения числа 1
         {
             return value1;
         }
 
-        public int getValue2()
+        public int getValue2()                      // Числа 2
         {
             return value2;
         }
     }
 }
+// На 64 строки меньше :)
